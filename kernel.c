63d0a201c140099c257b1eef69b91a1276098632
Xxmarijnw <marijnvanwezel@gmail.com>
2020-02-07 11:43:18 +0100

    Credits, rename some functions

--- kernel.c
+++ kernel.c
@@ -1,31 +1,152 @@
 /*
+* Copyright (C) 2014  Arjun Sreedharan
+* License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
+*/
+
+#include "keyboard_map.h"
+
+#define LINES 25
+#define COLUMNS_IN_LINE 80
+#define BYTES_FOR_EACH_ELEMENT 2
+#define SCREENSIZE BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE * LINES
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define IDT_SIZE 256
+#define INTERRUPT_GATE 0x8e
+#define KERNEL_CODE_SEGMENT_OFFSET 0x08
+#define ENTER_KEY_CODE 0x1C
+
+extern unsigned char keyboard_map[128];
+extern void keyboard_handler(void);
+extern char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+
+char *vidptr = (char*)0xb8000;
+unsigned int current_loc = 0;
+
+/*
 *  kernel-001.c
 */
+
+struct IDT_entry {
+    unsigned short int offset_lowerbits;
+    unsigned short int selector;
+    unsigned char      zero;
+    unsigned char      type_attr;
+    unsigned short int offset_higherbits;
+};
+
+/* create array of IDT_entry */
+struct IDT_entry IDT_entries[IDT_SIZE];
+
+void idt_init(void)
+{
+    unsigned long keyboard_address;
+    unsigned long idt_address;
+    unsigned long idt_ptr[2];
+
+    /* populate IDT entry of keyboard's interrupt */
+    keyboard_address = (unsigned long)keyboard_handler;
+    IDT_entries[0x21].offset_lowerbits = keyboard_address & 0xffff;
+    IDT_entries[0x21].selector = KERNEL_CODE_SEGMENT_OFFSET;
+    IDT_entries[0x21].zero = 0;
+    IDT_entries[0x21].type_attr = INTERRUPT_GATE;
+    IDT_entries[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+
+    write_port(0x20 , 0x11);
+    write_port(0xA0 , 0x11);
+    write_port(0x21 , 0x20);
+    write_port(0xA1 , 0x28);
+    write_port(0x21 , 0x00);
+    write_port(0xA1 , 0x00);
+    write_port(0x21 , 0x01);
+    write_port(0xA1 , 0x01);
+    write_port(0x21 , 0xff);
+    write_port(0xA1 , 0xff);
+
+    idt_address = (unsigned long)IDT_entries;
+    idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+    idt_ptr[1] = idt_address >> 16 ;
+
+    load_idt(idt_ptr);
+}
+
+void kb_init(void)
+{
+    write_port(0x21 , 0xFD);
+}
+
+/**
+ * Output a char pointer array to the screen.
+ *
+ * @param str
+ */
+void kernel_print(const char *str)
+{
+    unsigned int i = 0;
+    while(str[i] != '\0') {
+        vidptr[current_loc++] = str[i++];
+        vidptr[current_loc++] = 0x07;
+    }
+}
+
+/**
+ * Output a newline to the screen.
+ */
+void kernel_print_newline(void)
+{
+    unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
+    current_loc = current_loc + (line_size - current_loc % (line_size));
+}
+
+/**
+ * Clear the screen.
+ */
+void kernel_clear_screen(void)
+{
+    unsigned int i = 0;
+    while (i < SCREENSIZE) {
+        vidptr[i++] = ' ';
+        vidptr[i++] = 0x07;
+    }
+}
+
+void keyboard_handler_main(void)
+{
+    unsigned char status;
+    char keycode;
+
+    write_port(0x20, 0x20);
+
+    status = read_port(KEYBOARD_STATUS_PORT);
+    /* Lowest bit of status will be set if buffer is not empty */
+    if (status & 0x01) {
+        keycode = read_port(KEYBOARD_DATA_PORT);
+        if(keycode < 0) {
+            return;
+        }
+
+        if(keycode == ENTER_KEY_CODE) {
+            kernel_print_newline();
+            return;
+        }
+
+        vidptr[current_loc++] = keyboard_map[(unsigned char) keycode];
+        vidptr[current_loc++] = 0x07;
+    }
+}
+
 void kmain(void) {
     const char *str = "VimOS 0.0.0";
-    char *vidptr = (char*)0xb8000; 	//video mem begins here.
-    unsigned int i = 0;
-    unsigned int j = 0;
 
-    /* this loops clears the screen
-    * there are 25 lines each of 80 columns; each element takes 2 bytes */
-    while(j < 80 * 25 * 2) {
-        /* blank character */
-        vidptr[j] = ' ';
-        /* attribute-byte - light grey on black screen */
-        vidptr[j+1] = 0x07;
-        j = j + 2;
-    }
+    kernel_clear_screen();
+    kernel_print(str);
+    kernel_print_newline();
+    kernel_print_newline();
 
-    j = 0;
+    idt_init();
+    kb_init();
 
-    /* this loop writes the string to video memory */
-    while(str[j] != '\0') {
-        /* the character's ascii */
-        vidptr[i] = str[j];
-        /* attribute-byte: give character black bg and light grey fg */
-        vidptr[i+1] = 0x07;
-        ++j;
-        i = i + 2;
-    }
+    while(1);
 }
